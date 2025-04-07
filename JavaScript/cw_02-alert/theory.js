a67452f4334573dd49cc0e7c8c0e54cfd84d29e8
Andrey Yakovenko <yakovenkoandyua@gmail.com>
2025-04-07 17:59:54 +0300

    lesson 2 js

---
+++ JavaScript/cw_02-alert/theory.js
@@ -0,0 +1,31 @@
+// Number
+// Boolean - true/false
+// null - nothing 
+// undefined - empty
+// object = {}
+
+// BigInt
+// Symbol
+
+
+// let pass = '222';
+
+// const user = 'qweqwe';
+// var email = 'asdasd@ukr.net'
+
+
+// alert('Hello Roma!')
+
+// const userQuestion = confirm('ти прокинувся?')
+
+// console.log(userQuestion);
+
+let firstNum = prompt('enter number first')// 2
+
+let secondNum = prompt('enter number second')// 2
+
+let resultSumm = +firstNum + +secondNum // 2 + 2
+
+console.log(resultSumm); // 4
+
+console.log(Math.round(7.234234))
